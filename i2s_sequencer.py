0f7bec448edcde1e9f8eedefd14dbe2cba2cbaea
dash6424 <dash6424@colorado.edu>
2023-04-29 01:58:09 -0600

    test

--- i2s_sequencer.py
+++ i2s_sequencer.py
@@ -6,54 +6,59 @@ file_path = "audio_test.txt"
 song1_path = "test.wav"
 song2_path = "Front_Center.wav"
 
-# Get the initial modification time of the file
-last_mtime = os.stat(file_path).st_mtime
-
 # Initialize the flag variable to True
 continue_playing = True
 
 # Initialize the current song variable
 current_song = None
 
-while continue_playing:
-    # Check the modification time of the file
-    mtime = os.stat(file_path).st_mtime
+bool1 = False
+bool2 = False
+bool3 = False
 
-    # If the file has been modified since the last check, read it and get the last line
-    if mtime > last_mtime:
+while continue_playing:
         with open(file_path, "r") as f:
             lines = f.readlines()
             last_line = lines[-1].strip()
 
         # Check if the last line is a valid command
         if last_line == "1":
-            # Stop the current song, if there is one
-            if current_song is not None:
-                os.system("killall aplay")
-                time.sleep(1)  # wait for the previous song to stop
-
-            # Play the new song
-            current_song = song1_path
-            os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
+            bool2 = False
+            bool3 = False
+            if bool1 == False:
+                # Stop the current song, if there is one
+                if current_song is not None:
+                    os.system("killall aplay")
+                    time.sleep(1)  # wait for the previous song to stop
+                
+                # Play the new song
+                current_song = song1_path
+                os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
+                bool1 = True
 
         elif last_line == "2":
-            # Stop the current song, if there is one
-            if current_song is not None:
-                os.system("killall aplay")
-                time.sleep(1)  # wait for the previous song to stop
-
-            # Play the new song
-            current_song = song2_path
-            os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
+            bool1 = False
+            bool3 = False
+            if bool2 == False:
+                # Stop the current song, if there is one
+                if current_song is not None:
+                    os.system("killall aplay")
+                    time.sleep(1)  # wait for the previous song to stop
+                
+                # Play the new song
+                current_song = song2_path
+                os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
+                bool2 = True
 
         elif last_line == "3":
-            # Stop the current song, if there is one
-            if current_song is not None:
-                os.system("killall aplay")
-                current_song = None  # reset the current song variable
-
-        # Update the last modification time
-        last_mtime = mtime
+            bool1 = False
+            bool2 = False
+            if bool3 == False:
+                # Stop the current song, if there is one
+                if current_song is not None:
+                    os.system("killall aplay")
+                    current_song = None  # reset the current song variable
+                    bool3 = True
 
     # Wait for a few seconds before checking the file again
     time.sleep(2)

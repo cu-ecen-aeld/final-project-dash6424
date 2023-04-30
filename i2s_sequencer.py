ffb9e674f407cd16e20ede220b06c6658fc47866
dash6424 <dash6424@colorado.edu>
2023-04-29 18:48:29 -0600

    script updated

--- i2s_sequencer.py
+++ i2s_sequencer.py
@@ -2,63 +2,78 @@ import os
 import time
 
 # Define the file path and songs
+# Set the audio files
+songs = ["test1.wav", "test2.wav", "test3.wav", "test4.wav"]
 file_path = "audio_test.txt"
-song1_path = "test.wav"
-song2_path = "Front_Center.wav"
 
 # Initialize the flag variable to True
 continue_playing = True
 
 # Initialize the current song variable
 current_song = None
+idx = 0
 
-bool1 = False
-bool2 = False
-bool3 = False
+# Set the file to unknown state.
+with open(file_path, "w") as f:
+    f.write("9\n") 
 
 while continue_playing:
-        with open(file_path, "r") as f:
-            lines = f.readlines()
-            last_line = lines[-1].strip()
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+        last_line = lines[-1].strip()
 
-        # Check if the last line is a valid command
-        if last_line == "1":
-            bool2 = False
-            bool3 = False
-            if bool1 == False:
-                # Stop the current song, if there is one
-                if current_song is not None:
-                    os.system("killall aplay")
-                    time.sleep(1)  # wait for the previous song to stop
-                
-                # Play the new song
-                current_song = song1_path
-                os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
-                bool1 = True
+    # Check if the last line is a valid command
+    if last_line == "1":
+        # Stop the current song, if there is one
+        if current_song is not None:
+            os.system("killall aplay")
+            time.sleep(1)  # wait for the current song to stop
+        
+        # Play the new song
+        current_song = songs[idx]
+        os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
 
-        elif last_line == "2":
-            bool1 = False
-            bool3 = False
-            if bool2 == False:
-                # Stop the current song, if there is one
-                if current_song is not None:
-                    os.system("killall aplay")
-                    time.sleep(1)  # wait for the previous song to stop
-                
-                # Play the new song
-                current_song = song2_path
-                os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
-                bool2 = True
+    elif last_line == "2":
+        # Stop the current song, if there is one
+        if current_song is not None:                
+            os.kill(os.getpid(), 2)  # add "&" to run in the background
 
-        elif last_line == "3":
-            bool1 = False
-            bool2 = False
-            if bool3 == False:
-                # Stop the current song, if there is one
-                if current_song is not None:
-                    os.system("killall aplay")
-                    current_song = None  # reset the current song variable
-                    bool3 = True
+    elif last_line == "3":
+        # Stop the current song, if there is one
+        if current_song is not None:
+            os.system("killall aplay")
+            time.sleep(1)  # wait for the current song to stop
+        
+        # Play the previous song
+        idx = idx - 1
+        if idx < 0:
+            idx = 3
+        current_song = songs[idx]
+        os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
 
+    elif last_line == "4":
+        # Stop the current song, if there is one
+        if current_song is not None:
+            os.system("killall aplay")
+            time.sleep(1)  # wait for the current song to stop
+        
+        # Play the next song
+        idx = idx + 1
+        if idx == 4:
+            idx = 0
+        current_song = songs[idx]
+        os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
+
+    elif last_line == "5":
+        # Stop the current song, if there is one
+        if current_song is not None:
+            os.system("killall aplay")
+            current_song = None  # reset the current song variable
+            idx = 0
+
+    # Set the file to unknown state.
+    with open(file_path, "w") as f:
+        f.write("9\n")    
+    
     # Wait for a few seconds before checking the file again
     time.sleep(2)

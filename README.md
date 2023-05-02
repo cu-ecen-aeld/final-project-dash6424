c94a29adc53b99aff2e62de0a0d34ea796da71a5
dash6424 <dash6424@colorado.edu>
2023-05-01 18:18:54 -0600

    Added references and comments

--- README.md
+++ README.md
@@ -10,3 +10,22 @@ The overview is hosted at this [link](https://github.com/sachininja/final-projec
 
 # Project Schedule:
 The Project schedule is hosted at this [link](https://github.com/users/sachininja/projects/1/views/1?groupedBy%5BcolumnId%5D=39014094).
+
+
+# Description:
+This git hub link contains the database required to be loaded on the buildroot image.
+
+i2s configuration and audio Playback, refer to buildroot image:
+https://github.com/cu-ecen-aeld/final-project-sachininja/tree/i2s-buildroot
+
+Entire audio playback with bluetooth commands, refer to buildroot image:
+https://github.com/cu-ecen-aeld/final-project-sachininja/tree/integrated_build
+
+# Files:
+1. asound.conf - Contains the soundcard configuration details with default media format to be 44100kHz, 16bits per sample and stereo channel. Sets the i2s in master mode.
+2. config.txt - This is a bootup config file that is not required to be pushed on buildroot but has 3 important soundcard packages that need to be booted on rpi.
+dtoverlay=hifiberry-dac
+dtoverlay=i2s-mmap
+3. i2s_sequencer.py - Is the script that polls the audio_test.txt file to receive commands and plays digital data over i2s pins on rpi.
+4. max98357a - The hw codec that supports i2s to read digital data and play audio over speaker.
+5. test1.wav, test2.wav, test3.wav & test4.wav - are the 4 audio files that are loaded on the buildroot image to test audio functionality.
--- i2s_sequencer.py
+++ i2s_sequencer.py
@@ -1,20 +1,34 @@
+# Author: Daanish Shariff
+# Name: i2s_sequencer.py
+# Description: Audio play sequencer script. This script continuously polls the value at audio_test.txt file and executes the respective command. By default it writes a valid 9 to be the invalid command.
+# Commands:
+# 1 - Play
+# 2 - Previous
+# 3 - Next
+# 4 - Stop
+# 9 - Default invalid command
+# Date Modified: 05/01/2023
+# References: 
+# 1. File read write operation: https://www.guru99.com/reading-and-writing-files-in-python.html
+# 2. aplay command for audio playback: https://stackoverflow.com/questions/45136500/using-aplay-to-play-a-sound-through-subprocess-within-a-python-script
+# 3. File mutex lock: https://stackoverflow.com/questions/6931342/system-wide-mutex-in-python-on-linux
+
+########################################## Imports ################################################
 import os
 import fcntl
 import time
 
+########################################## Global Variables ################################################
 # Define the file path and songs
-# Set the audio files
 songs = ["/usr/bin/test1.wav", "/usr/bin/test2.wav", "/usr/bin/test3.wav", "/usr/bin/test4.wav"]
 file_path = "/usr/bin/audio_test.txt"
 
-# Initialize the flag variable to True
-continue_playing = True
-
-# Initialize the current song variable
+# Initialize the current song variable to hold current song
 current_song = None
+# idx of song offset in list
 idx = 0
 
-# Set the file to unknown state.
+# Set the file to default state.
 with open(file_path, "w") as f:
     fcntl.flock(f, fcntl.LOCK_EX)
     f.write("9\n") 
@@ -23,7 +37,7 @@ with open(file_path, "w") as f:
 # Wait for a few seconds before checking the file again
 time.sleep(2)
 
-while continue_playing:
+while True:
     with open(file_path, "r") as f:
         fcntl.flock(f, fcntl.LOCK_EX)
         lines = f.readlines()
@@ -31,7 +45,7 @@ while continue_playing:
         last_line = lines[-1].strip()
 
     # Check if the last line is a valid command
-    if last_line == "1":
+    if last_line == "1": # Play command
         # Stop the current song, if there is one
         if current_song is not None:
             os.system("killall aplay")
@@ -39,14 +53,14 @@ while continue_playing:
         
         # Play the new song
         current_song = songs[idx]
-        os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
-        # Set the file to unknown state.
+        os.system(f"aplay -q {current_song} &")
+        # Set the file to default state.
         with open(file_path, "w") as f:
             fcntl.flock(f, fcntl.LOCK_EX)
             f.write("9\n") 
             fcntl.flock(f, fcntl.LOCK_UN)
 
-    elif last_line == "2":
+    elif last_line == "2": # Previous command
         # Stop the current song, if there is one
         if current_song is not None:
             os.system("killall aplay")
@@ -58,13 +72,13 @@ while continue_playing:
             idx = 3
         current_song = songs[idx]
         os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
-        # Set the file to unknown state.
+        # Set the file to default state.
         with open(file_path, "w") as f:
             fcntl.flock(f, fcntl.LOCK_EX)
             f.write("9\n") 
             fcntl.flock(f, fcntl.LOCK_UN) 
 
-    elif last_line == "3":
+    elif last_line == "3": # Next command
         # Stop the current song, if there is one
         if current_song is not None:
             os.system("killall aplay")
@@ -76,13 +90,13 @@ while continue_playing:
             idx = 0
         current_song = songs[idx]
         os.system(f"aplay -q {current_song} &")  # add "&" to run in the background
-        # Set the file to unknown state.
+        # Set the file to default state.
         with open(file_path, "w") as f:
             fcntl.flock(f, fcntl.LOCK_EX)
             f.write("9\n") 
             fcntl.flock(f, fcntl.LOCK_UN)
 
-    elif last_line == "4":
+    elif last_line == "4": # Stop command
         # Stop the current song, if there is one
         if current_song is not None:
             os.system("killall aplay")
@@ -90,12 +104,12 @@ while continue_playing:
             idx = 0
         # Wait for a few seconds before checking the file again
         time.sleep(2)
-        # Set the file to unknown state.
+        # Set the file to default state.
         with open(file_path, "w") as f:
             fcntl.flock(f, fcntl.LOCK_EX)
             f.write("9\n") 
             fcntl.flock(f, fcntl.LOCK_UN)
-    else:
+    else: # Invalid command
         pass
     
     # Wait for a few seconds before checking the file again
